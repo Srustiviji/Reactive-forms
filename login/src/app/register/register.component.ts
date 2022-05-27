c15123892de173f91db8459e77691d1e769381bf
srustiviji <vijayalakshmi@sparkouttech.com>
2022-05-27 14:58:09 +0530

    first commit

---
+++ login/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+   registerForm:any;
+  ngOnInit(): void {
+      this.registerForm=new FormGroup({
+      "firstName":new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z]*')]),
+      "lastName":new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z]*')]),
+      "emailid":new FormControl(null,[Validators.required,Validators.email]),
+      "phonenumber":new FormControl(null,[Validators.required,Validators.pattern('[0-9]')])
+    })
+  }
+  submitData()
+
+  {console.log(this.registerForm.value);
+  }
+  get firstname() {return this.registerForm.get('firstName')};
+  get lastname() {return this.registerForm.get('lastName')};
+  get emailid() {return this.registerForm.get('emailid')};
+  get phonenumber() {return this.registerForm.get('phonenumber')};
+}
+
+
+
